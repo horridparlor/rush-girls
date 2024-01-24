9f388549b1f2f7cd4cb0a272f12066c10fc42b2b
Eero Laine <horridparlormaster@gmail.com>
2024-01-24 17:49:31 +0200

    fucked, accidentally upped pasword

--- api/system/Database.php
+++ api/system/Database.php
@@ -4,9 +4,9 @@ class  Database
 {
     function connect()
     {
-        $servername = "31.217.196.118";
-        $username = "zdccdlji_rushgirls";
-        $password = "FformatSsetters5%Laventel";
+        $servername = "ip_address_here";
+        $username = "table_name_same_as_username_here";
+        $password = "user_password_here";
         $connection = new mysqli($servername, $username, $password, $username);
         if ($connection->connect_error) {
             die("Failed to connect DATABASE: " . $connection->connect_error);
