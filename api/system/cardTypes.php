a4e8aeb543c03ee55e8b517207da41208ce076c8
Eero Laine <horridparlormaster@gmail.com>
2024-02-02 13:35:34 +0200

    Played with nuke-game idea

--- api/system/cardTypes.php
+++ api/system/cardTypes.php
@@ -2,12 +2,12 @@
 const CARD_TYPE_MONSTER = 8;
 const CARD_TYPE_BACKROW = 9;
 const CARD_TYPE_NORMAL = 1;
-const CARD_TYPE_EFFECT = 7;
-const CARD_TYPE_SPELL = 2;
-const CARD_TYPE_TRAP = 3;
-const CARD_TYPE_FUSION = 4;
+const CARD_TYPE_EFFECT = 2;
+const CARD_TYPE_SPELL = 3;
+const CARD_TYPE_TRAP = 4;
+const CARD_TYPE_FUSION = 5;
 const CARD_TYPE_REVENGE = 6;
-const CARD_TYPE_RITUAL = 5;
+const CARD_TYPE_RITUAL = 7;
 const DECK_MAIN = 1;
 const DECK_EXTRA = 2;
 const CARD_TYPE_IN = " AND card.type_id in ";
